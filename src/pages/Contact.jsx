ce3d6fccff1701f0bd5295c0a0b989b16cebcc17
afyww <sivinaries@gmail.com>
2025-03-01 23:02:25 +0700

    upp

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -12,7 +12,7 @@ function Contact() {
             <div className='w-full mx-auto'>
                 <Hero />
                 <Hubungi />
-                <div className="hidden md:flex">
+                <div className="hidden md:flex px-6 mb-4 md:p-10 md:mb-0">
                 <Mitra />
                 </div>
                 <div className="flex md:hidden">
