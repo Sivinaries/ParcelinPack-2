24169aef4b5368608362ffe6d57e1d75e1c7633d
Alvin Yusuf Riziq <alvinysfriz1@std.unissula.ac.id>
2025-03-01 21:47:08 +0700

    Add Mitra component with image and promotional text to Contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,24 +3,21 @@ import Hubungi from "../components/Contact/Hubungi";
 import Footer from "../components/Footer";
 import Mitra from "../components/Mitra";
 import Navbar from "../components/Navbar";
-import Promote from '../components/Promote'
 
 function Contact() {
-    return (
-        <main className='flex flex-col'>
-            <Navbar />
-            <div className='w-full mx-auto'>
-                <Hero />
-                <Hubungi />
-                <div className="hidden md:flex">
-                <Mitra />
-                </div>
-                <div className="flex md:hidden">
-                <Promote />
-                </div>
-            </div>
-            <Footer />
-        </main>)
+  return (
+    <main className="flex flex-col">
+      <Navbar />
+      <div className="w-full mx-auto">
+        <Hero />
+        <Hubungi />
+        <div className="px-6 mb-4 md:p-10 md:mb-0">
+          <Mitra />
+        </div>
+      </div>
+      <Footer />
+    </main>
+  );
 }
 
-export default Contact
+export default Contact;
